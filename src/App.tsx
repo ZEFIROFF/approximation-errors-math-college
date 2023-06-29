770089f9d910f07f7989b6ce3ea282c21e645c7d
ZEFIROFF <d3piis@gmail.com>
2023-06-30 01:10:28 +0400

    init second

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+// @ts-ignore
+import formula from './image/Screenshot_6.png'
 
 function App() {
+  const [a, setA] = useState(2.03);
+  const [b, setB] = useState(-1.670);
+  const [x, setX] = useState(0.970);
+  const [y, setY] = useState(0.504);
+
+  const calculate = () => {
+    const result = (a * b - Math.exp(x)) / Math.sin(y);
+    return result.toFixed(4);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="calculator">
+        <h1>Вычисление погрешностей результатов арифметических действий над приближёнными числами</h1>
+        <h2>Пусть а, b, у – приближенные числа с верными в строгом смысле значащими цифрами, х – точное число. Вычислить <img
+            src={formula} alt="(a*b-e^x)/sin(y)"/> и оценить погрешность результата.
+        </h2>
+        </div>
+        <div className="input-group">
+        <label>
+          a:
+          <input type="number" value={a} onChange={e => setA(+e.target.value)} />
+        </label>
+        <label>
+          b:
+          <input type="number" value={b} onChange={e => setB(+e.target.value)} />
+        </label>
+        <label>
+          x:
+          <input type="number" value={x} onChange={e => setX(+e.target.value)} />
+        </label>
+        <label>
+          y:
+          <input type="number" value={y} onChange={e => setY(+e.target.value)} />
+        </label>
+        </div>
+        <h2>Result: {calculate()}</h2>
+      </div>
     </div>
   );
 }
